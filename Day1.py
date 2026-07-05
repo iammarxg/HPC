175fa5dd7e3e7e471482cdd37e8b42b38eec10d9
Ammar Alghamdi <iammarxgames@gmail.com>
2026-07-05 11:18:05 +0300

    Day1

---
+++ Day1.py
@@ -0,0 +1,18 @@
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+load_dotenv()
+
+client = genai.Client()
+
+response = client.models.generate_content(
+    model="gemini-3.1-flash-lite",
+    contents="Explain how AI works in a single sentence.",
+    config=types.GenerateContentConfig(
+        max_output_tokens=100,
+        temperature=0.5,
+    )
+)
+
+print(response.text)
